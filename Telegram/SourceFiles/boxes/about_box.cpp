a330eee40a04d6927e5a18df3578a4b0a20ddd6b
onysd <beatvin128@gmail.com>
2026-03-07 00:10:05 +0200

    changed name suffix logic

--- Telegram/SourceFiles/boxes/about_box.cpp
+++ Telegram/SourceFiles/boxes/about_box.cpp
@@ -151,6 +151,7 @@ QString telegramFaqLink() {
 
 QString currentVersionText() {
 	auto result = QString::fromLatin1(AppVersionStr);
+	result += "_OD";
 	if (cAlphaVersion()) {
 		result += u" alpha %1"_q.arg(cAlphaVersion() % 1000);
 	} else if (AppBetaVersion) {
